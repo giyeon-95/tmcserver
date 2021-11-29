c339ab591130448d623850a9dfa49775fd5bb73c
giyeon-95 <68680114+giyeon-95@users.noreply.github.com>
2021-11-29 15:35:35 +0900

    -

--- routes/engine/index.js
+++ routes/engine/index.js
@@ -26,7 +26,7 @@ const upload = multer({
       cacheControl: 'max-age=31536000',
       key: function (req, file, cb) {
         const extname = path.extname(file.originalname);
-        const filename = hash(file.stream);
+        const filename = file.stream;
         const sorceFilename = `files/${filename}${extname}`;
         cb(null, sorceFilename);
       },
