7e21ad4c63aac4beb5e62b63b7d0e189b3ca69f0
giyeon-95 <68680114+giyeon-95@users.noreply.github.com>
2021-11-29 10:42:24 +0900

    -

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use(jwtMiddleware);
-app.use('/', indexRouter);
+app.use('/api', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
