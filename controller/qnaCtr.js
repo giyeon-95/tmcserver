a11707549524e39462e2d4a19f030ca6d54e2e3a
giyeon-95 <68680114+giyeon-95@users.noreply.github.com>
2021-11-30 16:20:31 +0900

    -

--- controller/qnaCtr.js
+++ controller/qnaCtr.js
@@ -39,7 +39,7 @@ const qnaCtr = {
         title : title, 
         content : content,
         answered : false, 
-        comment : '',
+        comments : '',
     })
 
     await qna.save().then((_) => {
