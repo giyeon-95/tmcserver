fc44019fbf292e7ada8b3d1cf0103fa36fddb25e
giyeon-95 <68680114+giyeon-95@users.noreply.github.com>
2021-12-28 16:27:48 +0900

    -

--- model/engine.js
+++ model/engine.js
@@ -1,11 +1,22 @@
 const mongoose = require('mongoose');  
 const Schema = mongoose.Schema; 
+const autoIncrement = require('mongoose-auto-increment');
+
+autoIncrement.initialize(mongoose.connection);
 
 const engineSchema = new Schema ({
+    seq : Number,
     useremail : String, 
     title : String, 
     content : String,
     file : String,
-},{timestamps : true, versionKey : false})
+},{timestamps : true, versionKey : false});
+
+engineSchema.plugin(autoIncrement.plugin, {
+    model : "engine",
+    field : "seq", 
+    startAt : 1,
+    increment : 1, 
+});
 
 module.exports= mongoose.model('engine', engineSchema);
--- model/notice.js
+++ model/notice.js
@@ -1,10 +1,21 @@
 const mongoose = require('mongoose');  
 const Schema = mongoose.Schema; 
+const autoIncrement = require('mongoose-auto-increment');
+
+autoIncrement.initialize(mongoose.connection);
 
 const noticeSchema = new Schema ({
+    seq : Number,
     useremail : String, 
     title : String,
     content : String,
 },{timestamps : true, versionKey : false});
+
+noticeSchema.plugin(autoIncrement.plugin, {
+    model : "notice",
+    field : "seq", 
+    startAt : 1,
+    increment : 1, 
+});
 
 module.exports= mongoose.model('notice', noticeSchema);
